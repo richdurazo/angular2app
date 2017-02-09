f0b3afe3351410cf17a53e09fd1c1356954c3bc3
Richard Durazo <rcdurazo@gmail.com>
2017-02-08 22:31:52 -0800

    added custom event

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Message } from "./messages/message.model"
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+
 })
 export class AppComponent {
-    message= {
-        content: 'a message',
-        author: 'rich'
-    };
+    message: Message = new Message('Some message', 'Rich');
 }

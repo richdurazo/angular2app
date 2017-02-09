066cada07562e34ccdeb63d23bd369ee157f3b3a
Richard Durazo <rcdurazo@gmail.com>
2017-02-08 18:45:06 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// holds the code that is executed on each request reaching our server
+// this file is reached with any request.
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var appRoutes = require('./routes/app');
+
+var app = express();
+mongoose.connect('localhost:27017/node-angular');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/', appRoutes);
+
+// catch 404 and forward to error handler
+// if we try to access a route that is not defined in routes/app.js --> 404 errors. normally you would show error page. 
+// makes sure that we always render angular 2 application because our routes are set up there.
+app.use(function (req, res, next) {
+    return res.render('index');
+});
+
+
+module.exports = app;
